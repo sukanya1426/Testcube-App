39b6ab1ee644cb47fb90112ccb847c26398b5757
sukanya1426 <rahmanmahdiya2002@gmail.com>
2025-02-24 14:08:55 +0600

    Switched the project from Nextjs to Reactjs

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='h-8 w-full bg-red-500'>Hello World</div>
+  )
+}
+
+export default App
--- src/app/sso-callback/sso-callback/[[...rest]]/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function SsoCallbackPage() {
-    return (
-      <div className="flex items-center justify-center w-full min-h-screen">
-        <h1>Loading</h1>
-        {/* You can add a loader or handle specific SSO logic here */}
-      </div>
-    );
-  }
-  
